31c5dd541ff6a3e72bcfd4deea0b941364a5ab51
German Fica <germafica@gmail.com>
2025-08-18 15:25:53 -0300

    Add demo_webcam.py

--- README.md
+++ README.md
@@ -5,10 +5,17 @@
 - Python 3.10.0
 - Mediapipe 0.10.5
 
-## Run
+## Install
 
 ```
 python -m venv .venv
 .venv\Scripts\activate
 pip install mediapipe==0.10.5
+pip install opencv-python==4.10.0.84
+```
+
+## Run
+
+```
+python demo_webcam.py
 ```
---
+++ demo_webcam.py
@@ -0,0 +1,43 @@
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+# Abrir webcam
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # Usar CAP_DSHOW para evitar problemas con la cámara en Windows
+
+if not cap.isOpened():
+    print("No se pudo abrir la cámara 0")
+else:
+    print("Cámara abierta con éxito")
+cap.release()
+
+with mp_pose.Pose(min_detection_confidence=0.5,
+                  min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Convertir a RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        # Procesar con MediaPipe
+        results = pose.process(image)
+
+        # Dibujar resultados sobre la imagen original
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.pose_landmarks:
+            mp_drawing.draw_landmarks(
+                image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        cv2.imshow('MediaPipe Pose Demo', image)
+
+        if cv2.waitKey(5) & 0xFF == 27:  # Esc para salir
+            break
+
+cap.release()
+cv2.destroyAllWindows()
